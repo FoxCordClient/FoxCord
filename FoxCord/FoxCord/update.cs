5278604fcc831fd69ad0297bfa14260f1b522827
PshNsDev <durkmcteam@gmail.com>
2026-07-25 21:45:36 -0400

    Source V0.7.3

--- FoxCord/FoxCord/update.cs
+++ FoxCord/FoxCord/update.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -8,12 +9,11 @@ namespace FoxCord
 {
     public static class UpdateChecker
     {
-        public static readonly string CurrentVersionStr = "0.7.2";
+        public static readonly string CurrentVersionStr = "0.7.3";
 
         private const string GitHubApiUrl = "https://api.github.com/repos/PshNsDev/FoxCord/releases/latest";
+        private const string ReleasesUrl = "https://github.com/PshNsDev/FoxCord/releases/latest";
 
-        /// <summary>
-        /// </summary>
         public static async Task CheckForUpdatesAsync()
         {
             try
@@ -24,11 +24,10 @@ namespace FoxCord
                 var response = await client.GetAsync(GitHubApiUrl);
 
                 if (!response.IsSuccessStatusCode)
-                {
                     return;
-                }
 
                 string jsonContent = await response.Content.ReadAsStringAsync();
+
                 using JsonDocument doc = JsonDocument.Parse(jsonContent);
 
                 if (doc.RootElement.TryGetProperty("tag_name", out JsonElement tagElement))
@@ -40,14 +39,14 @@ namespace FoxCord
             catch (HttpRequestException)
             {
                 MessageBox.Show(
-                    "Unable to check for updates. Please check your Wi-Fi or internet connection.",
+                    "Unable to check for updates.\nPlease check your internet connection.",
                     "FoxCord - Connection Error",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine($"Update check error: {ex.Message}");
+                Debug.WriteLine(ex);
             }
         }
 
@@ -66,21 +65,75 @@ namespace FoxCord
 
             if (result < 0)
             {
-                MessageBox.Show(
-                    $"Hey, Updates out you are on {localVerStr} and the latest version is {onlineTag}",
-                    "FoxCord - Update Available",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
+                bool forceUpdate = IsForceUpdate(localVersion, onlineVersion);
+
+                if (forceUpdate)
+                {
+                    while (true)
+                    {
+                        DialogResult dialog = MessageBox.Show(
+                            $"Your FoxCord version ({localVerStr}) is too old.\n\n" +
+                            $"Latest version: {onlineTag}\n\n" +
+                            "OK = Update Now\n" +
+                            "Cancel = Not Allowed",
+                            "FoxCord - Update Required",
+                            MessageBoxButtons.OKCancel,
+                            MessageBoxIcon.Warning);
+
+                        if (dialog == DialogResult.OK)
+                        {
+                            Process.Start(new ProcessStartInfo
+                            {
+                                FileName = ReleasesUrl,
+                                UseShellExecute = true
+                            });
+
+                            Application.Exit();
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    DialogResult dialog = MessageBox.Show(
+                        $"A new version of FoxCord is available!\n\n" +
+                        $"Current Version: {localVerStr}\n" +
+                        $"Latest Version: {onlineTag}\n\n" +
+                        "OK = Update Now\n" +
+                        "Cancel = Update Later",
+                        "FoxCord - Update Available",
+                        MessageBoxButtons.OKCancel,
+                        MessageBoxIcon.Information);
+
+                    if (dialog == DialogResult.OK)
+                    {
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName = ReleasesUrl,
+                            UseShellExecute = true
+                        });
+                    }
+                }
             }
             else if (result > 0)
             {
                 MessageBox.Show(
-                    "Hey, you recompiled the FoxCord right? or not? hm idk",
+                    "You're using a newer/development build of FoxCord.",
                     "FoxCord",
                     MessageBoxButtons.OK,
-                    MessageBoxIcon.Question);
+                    MessageBoxIcon.Information);
             }
-            
+        }
+
+        private static bool IsForceUpdate(Version current, Version latest)
+        {
+            if (latest.Major > current.Major)
+                return true;
+
+            if (latest.Minor - current.Minor >= 2)
+                return true;
+
+            return false;
         }
     }
 }
